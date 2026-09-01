7e5e7b4d3742a16e20286e5263c118d500efaa50
Syntaxxxxxxxxx <wenrui@hust.edu.cn>
2026-09-01 21:26:50 +0800

    feat: add embedding SiLU and SwiGLU modules

--- cs336_basics/network.py
+++ cs336_basics/network.py
@@ -1,6 +1,22 @@
 import torch
 from torch import nn
 import math
+from einops import einsum
+
+def SiLU(x):
+    return x*torch.sigmoid(x)
+
+class Embedding(nn.Module):
+    def __init__(self,num_embeddings,d_model,device=None,dtype=None):
+        super().__init__()
+        self.num_embeddings=num_embeddings
+        self.d_model=d_model
+        para=torch.empty((num_embeddings,d_model),dtype=dtype,device=device)
+        torch.nn.init.trunc_normal_(para,mean=0,std=1,a=-3,b=3)
+        self.weight=nn.Parameter(para)
+    def forward(self,x):
+        return self.weight[x]
+
 
 class Linear(nn.Module):
     def __init__(self,in_dim,out_dim,device=None,dtype=None):
@@ -30,3 +46,18 @@ class RMSNorm(nn.Module):
         rms=(sqr+self.eps)**0.5
         result=self.weight*x/rms
         return result.to(tp)
+
+class SwiGLU(nn.Module):
+    def __init__(self,d_model,d_ff,device=None,dtype=None):
+        super().__init__()
+        self.d_model=d_model
+        self.d_ff=d_ff
+        self.w1=Linear(d_model,d_ff,device=device,dtype=dtype)
+        self.w3=Linear(d_model,d_ff,device=device,dtype=dtype)
+        self.w2=Linear(d_ff,d_model,device=device,dtype=dtype)
+    def forward(self,x):
+        result=self.w1(x)
+        result=SiLU(result)
+        result*=self.w3(x)
+        result=self.w2(result)
+        return result
