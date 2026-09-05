2057da1c7ea9a71446a0289bc834807254bf32b2
Syntaxxxxxxxxx <wenrui@hust.edu.cn>
2026-09-06 03:31:27 +0800

    add training utilities and checkpointing

--- cs336_basics/llm_train.py
+++ cs336_basics/llm_train.py
@@ -3,6 +3,8 @@ from torch.utils.data import Dataset,DataLoader
 import torch
 from einops import rearrange
 from torch import optim 
+import math
+
 def log_sum_exp(x,dim):
     m,_=torch.max(x,dim=dim,keepdim=True)
     s=x-m
@@ -22,6 +24,52 @@ def cross_entropy_loss(x,targets):
     loss=lse-loss_all
     loss=torch.mean(loss)
     return loss
+
+def lr_schedule(t,alpha_max,alpha_min,T_w,T_c):
+    assert t>=0
+    if 0<=t<=T_w:
+        lr=alpha_max*t/T_w
+    elif T_w<=t<=T_c:
+        lr=alpha_min+(alpha_max-alpha_min)*(1+math.cos((math.pi)*(t-T_w)/(T_c-T_w)))/2
+    else:
+        lr=alpha_min
+    return lr
+
+def gradient_clip(params,max_l2_norm):
+    l2_norm=0
+    params=list(params)
+    for p in params:
+        if p.grad is not None:
+            grad=p.grad
+            grad=grad**2
+            l2_norm+=torch.sum(grad)
+    l2_norm=l2_norm**0.5
+    s=1
+    if l2_norm>max_l2_norm:
+        s=max_l2_norm/l2_norm
+    for p in params:
+        if p.grad is not None:
+            p.grad*=s
+    return None
+
+def save_ckpt(model,optimizer,iteration,out):
+    model_state=model.state_dict()
+    optimizer_state=optimizer.state_dict()
+    ckpt={
+        "model":model_state,
+        "optimizer":optimizer_state,
+        "iteration":iteration
+    }
+    torch.save(ckpt,out)
+
+def load_ckpt(src,model,optimizer):
+    ckpt=torch.load(src)
+    model_state=ckpt["model"]
+    optimizer_state=ckpt["optimizer"]
+    model.load_state_dict(model_state)
+    optimizer.load_state_dict(optimizer_state)
+    
+    return ckpt["iteration"]
 
 class AdamW(optim.Optimizer):
     def __init__(self,params,lr,betas=(0.9,0.999),eps=0.0001,weight_decay=0.001):
